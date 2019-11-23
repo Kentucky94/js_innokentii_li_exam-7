b4cf3b8ca274e8541757e8f1f90109a461c8dd12
Kentucky94 <lis5043@gmail.com>
2019-11-23 12:23:37 +0600

    Item menu added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import nanoid from 'nanoid';
+
+import MenuItem from "./Components/MenuItem/MenuItem";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import HamburgerImage from './assets/hamburger.jpg';
+import CheeseburgerImage from './assets/cheeseburger.png';
+import FriesImage from './assets/fries.jpeg';
+import TeaImage from './assets/tea.jpeg';
+import CoffeeImage from './assets/coffee.jpeg';
+import ColaImage from './assets/cola.jpeg';
 
-    </div>
-  );
+const menu = [
+    {name: 'Hamburger', price: 80, image: HamburgerImage},
+    {name: 'Cheeseburger', price: 90, image: CheeseburgerImage},
+    {name: 'Fries', price: 45, image: FriesImage},
+    {name: 'Coffee', price: 70, image: CoffeeImage},
+    {name: 'Tea', price: 50, image: TeaImage},
+    {name: 'Cola', price: 40, image: ColaImage},
+];
+
+class App extends Component {
+    state = {
+
+    };
+
+    render() {
+        const menuItems = menu.map(item => <MenuItem key={nanoid()} name={item.name} price={item.price} image={item.image}/>);
+
+        return (
+            <div className="App container">
+                <div className="window leftWindow">
+
+                </div>
+                <div className="window rightWindow">
+                    {menuItems}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Components/MenuItem/MenuItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './MenuItem.css';
+
+const MenuItem = props => {
+    return (
+        <div className='menuItem_container'>
+            <img className='menuItem_image' src={props.image} alt="img"/>
+            <div>
+                <h5>{props.name}</h5>
+                <p>Price: {props.price} KGS</p>
+            </div>
+        </div>
+    );
+};
+
+export default MenuItem;
